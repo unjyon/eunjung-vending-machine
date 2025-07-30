f6e3e9bed8dea3b247e466eaca74fd0ac1ff6fb8
EunjungSong <unjyon.song@gmail.com>
2025-07-30 17:03:11 +0900

    feat: 음료 박스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 
+interface Drink {
+  id: string;
+  name: string;
+  price: number;
+  stock: number;
+}
+
 const cashArr = [100, 500, 1000, 5000, 10000];
+// 초기 자판기 음료 데이터
+const initialDrinks: Drink[] = [
+  { id: "cola", name: "콜라", price: 1100, stock: 5 },
+  { id: "water", name: "물", price: 600, stock: 10 },
+  { id: "coffee", name: "커피", price: 700, stock: 7 },
+];
 
 function App() {
+  const [message, setMessage] = useState("돈을 넣어주세요.");
+  const [insertedAmount, setInsertedAmount] = useState(0); // 투입금액
+  const [change, setChange] = useState(0); // 거스름돈
+  const [drinks, setDrinks] = useState<Drink[]>(initialDrinks);
+
   return (
     <div className="vending-machine-container">
       <h1>간이 자판기</h1>
 
       <div className="display-panel">
-        <p className="message">돈을 넣어주세요.</p>
+        <p className="message">{message}</p>
 
-        <p className="amount">투입 금액: 10000원</p>
+        <p className="amount">투입 금액: {insertedAmount}원</p>
 
         <p className="change-message">
-          반환할 거스름돈: 0원
-          <button>거스름돈 받기</button>
+          반환할 거스름돈: {change}원<button>거스름돈 받기</button>
         </p>
       </div>
 
@@ -30,7 +48,13 @@ function App() {
 
       <div className="drink-selection-section">
         <h2>음료 선택</h2>
-        <div className="drink-grid"></div>
+        <div className="drink-grid">
+          {drinks.map((drink) => (
+            <button key={drink.id} className="drink-button">
+              {drink.name} {drink.price}원<span>{drink.stock}개</span>
+            </button>
+          ))}
+        </div>
       </div>
 
       <div className="shopping-cart-section">
